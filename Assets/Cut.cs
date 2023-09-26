31db3fb2ead5e1e1dd61d6772d45aa844782a811
elan_rubin <elandanielrubin@gmail.com>
2023-09-26 19:02:06 -0400

    updated cut

--- Assets/Cut.cs
+++ Assets/Cut.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Cut : MonoBehaviour
 {
     [SerializeField] private List<Sprite> cuts = new();
+    List<SpriteRenderer> cutRenderers = new();
     void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = cuts[Random.Range(0, cuts.Count)];
+        foreach (Transform child in transform)
+            cutRenderers.Add(child.GetComponent<SpriteRenderer>());
+        var chosenSprite = cuts[Random.Range(0, cuts.Count)];
+        foreach (var r in cutRenderers)
+            r.sprite = chosenSprite;
+        //GetComponent<SpriteRenderer>().sprite = cuts[Random.Range(0, cuts.Count)];
     }
 
     void Update()
