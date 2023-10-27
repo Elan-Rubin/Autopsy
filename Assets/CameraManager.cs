868b779558d224ed954773f56ecf3a1140904c5e
elan_rubin <elandanielrubin@gmail.com>
2023-10-27 16:01:59 -0400

    soundmanager

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Unity.Mathematics;
 //using System.Numerics;
 
 public class CameraManager : MonoBehaviour
